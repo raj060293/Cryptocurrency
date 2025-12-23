12f2d41943dd427c1c10a9c10219e2d86fd5e989
babarajguru <baba170223@gmail.com>
2025-12-23 22:24:12 +0530

    Changes for miner and transaction input class

--- src/main/java/com/web3/cryptocurrency/Miner.java
+++ src/main/java/com/web3/cryptocurrency/Miner.java
@@ -1,4 +1,32 @@
 package com.web3.cryptocurrency;
 
+import com.web3.blockchain.Block;
+import com.web3.blockchain.Blockchain;
+import com.web3.constants.Constants;
+
 public class Miner {
+
+    private double reward;
+
+    public void mine(Block block, Blockchain blockchain) {
+
+        while(!isGoldenHash(block)) {
+            block.incrementNonce();
+            block.generateHash();
+        }
+
+        System.out.println(block + " has just been mined");
+        System.out.println("Hash is  " + block.getHash());
+        blockchain.addBlock(block);
+        reward += Constants.REWARD;
+    }
+
+    private boolean isGoldenHash(Block block) {
+        String leadingZeroes = new String(new char[Constants.DIFFICULTY]).replace('\0', '0');
+        return block.getHash().substring(0, Constants.DIFFICULTY).equals(leadingZeroes);
+    }
+
+    public double getReward() {
+        return reward;
+    }
 }
--- src/main/java/com/web3/cryptocurrency/TransactionInput.java
+++ src/main/java/com/web3/cryptocurrency/TransactionInput.java
@@ -1,4 +1,28 @@
 package com.web3.cryptocurrency;
 
 public class TransactionInput {
+
+    private String transactionOutputId;
+
+    private TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public String getTransactionOutputId() {
+        return transactionOutputId;
+    }
+
+    public void setTransactionOutputId(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public TransactionOutput getUTXO() {
+        return UTXO;
+    }
+
+    public void setUTXO(TransactionOutput UTXO) {
+        this.UTXO = UTXO;
+    }
 }
